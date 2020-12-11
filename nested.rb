694b323adcf68d40046528da6aadda25959c9469
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-11 09:10:03 -0600

    commit tree 4d3cc979d0375a4b257b1be052ef4ccaf8895f28

--- nested.rb
+++ nested.rb
@@ -15,13 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:grace_hopper]
 
 end
 
-def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+def alan_kay_is_known_for	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -79,8 +79,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     programmer_hash[:yukihiro_matsumoto] = {
+       :known_for => "Ruby",
+       :languages => ["LISP", "C"]
+       }
+     
+      programmer_hash
 end
 
 def changing_alan
@@ -101,8 +105,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -122,4 +126,6 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 end
